10dc793b6a10be86bf91060a8f54035fd86b7424
Conan Sun <125001930@qq.com>
2015-11-18 23:23:50 +0800

    add take screenshot function in the ChromeWebDriver

--- src/main/java/com/titan/conan/selenium/test/LoginTest.java
+++ src/main/java/com/titan/conan/selenium/test/LoginTest.java
@@ -1,11 +1,21 @@
 package com.titan.conan.selenium.test;
 
+import java.io.File;
+import java.io.IOException;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.LinkedHashMap;
+import java.util.Random;
 import java.util.concurrent.TimeUnit;
+
+import org.apache.commons.io.FileUtils;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 import org.openqa.selenium.By;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 
@@ -19,35 +29,40 @@ import junit.framework.TestCase;
 public class LoginTest extends TestCase {
 
 	private WebDriver driver;
-	private ISeleniumUtils utils;
-	private static final String WEB_DRIVER_NAME = "CHROME"; //PHANTOMJS : CHROME
+	private ISeleniumUtils seleniumUtils;
+	private static final String WEB_DRIVER_NAME = "PHANTOMJS"; //PHANTOMJS : CHROME
 	@Before
 	public void setUp() throws Exception {
 		if(WEB_DRIVER_NAME.equals("CHROME")) {
-			 utils = new ChromeWebDriver();
-		        driver = utils.getWebDriver("src/main/resources/chromedriver.exe");
+			 seleniumUtils = new ChromeWebDriver();
+		     driver = seleniumUtils.getWebDriver("src/main/resources/chromedriver.exe");
 		}else {
-			utils = new PhantomJSWebDriver();
-	        driver = utils.getWebDriver("src/main/resources/phantomjs.exe");
+			seleniumUtils = new PhantomJSWebDriver();
+	        driver = seleniumUtils.getWebDriver("src/main/resources/phantomjs.exe");
 		}
 		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
 	}
 	
 	@Test
-	public void testOpenBaidu() {
+	public void testOpenBaidu() throws IOException {
 		driver.get("http://www.baidu.com/");
 		System.out.println(driver.getTitle());
 		driver.manage().window().maximize();
-		WebElement loginLink = utils.findElementByLinkText("登录");
+		WebElement loginLink = seleniumUtils.findElementByLinkText("登录");
 		loginLink.click();
 		LinkedHashMap<By,String> inputFiledsInfo = new LinkedHashMap<By,String>();
 		inputFiledsInfo.put(By.id("TANGRAM__PSP_8__userName"), "nan125001930");
 		inputFiledsInfo.put(By.id("TANGRAM__PSP_8__password"), "sxn_125001930");
-		utils.typeInputFieldsWithClear(inputFiledsInfo);
-		WebElement loginBtn = utils.findElementById("TANGRAM__PSP_8__submit");
+		seleniumUtils.typeInputFieldsWithClear(inputFiledsInfo);
+		WebElement loginBtn = seleniumUtils.findElementById("TANGRAM__PSP_8__submit");
 		loginBtn.click();
-		Assert.assertTrue(utils.elementExist(By.className("mine-text")));
+		Assert.assertTrue(seleniumUtils.elementExist(By.className("mine-text")));
+		if(seleniumUtils instanceof ChromeWebDriver ) {
+			((ChromeWebDriver) seleniumUtils).takeScreenshot("src/main/resources/screenshots/");
+		}
 	}
+	
+	
 	
 	@After
 	public void tearDown() {
--- src/main/java/com/titan/conan/selenium/webdrivers/ChromeWebDriver.java
+++ src/main/java/com/titan/conan/selenium/webdrivers/ChromeWebDriver.java
@@ -1,5 +1,15 @@
 package com.titan.conan.selenium.webdrivers;
 
+import java.io.File;
+import java.io.IOException;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Random;
+
+import org.apache.commons.io.FileUtils;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeDriverService;
@@ -19,4 +29,15 @@ public class ChromeWebDriver extends AbstractSeleniumUtils{
 		return driver;
 	}
 	
+	public void takeScreenshot(String screenshotDir) throws IOException{
+		java.io.File screenshot = ((TakesScreenshot)driver).getScreenshotAs(OutputType.FILE);
+		String fileName = screenshotDir + getScreenshotFileName();
+		FileUtils.copyFile(screenshot,new File(fileName));
+	}
+	
+	private String getScreenshotFileName() {
+		DateFormat df = new SimpleDateFormat("yyyyMMddHHmmss");
+		Random random = new Random();
+		return df.format(new Date())+ + random.nextInt(10000)+".png";
+	}
 }
